7c618655dbfd9a790f9143b73b432607ae85418d
darkstalkerhansen-jpg <darkstalkerhansen@gmail.com>
2026-05-16 12:32:31 -0600

    test 1

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function flashObserver() {
 }
 
 function startGame() {
-  window.alert("version 0.133");
+  window.alert("version 0.134");
  if (confirm("are you in phone? press cancel if no and press ok for yes.")) {
    
  
