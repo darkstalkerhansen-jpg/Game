b5b221b4737475c583cc8200a2d7af77688f7361
darkstalkerhansen-jpg <darkstalkerhansen@gmail.com>
2026-05-18 12:32:00 -0600

    test 10

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let WASD = true;
 let RUN = false;
 let headBobTimer = 0;
 let INTERACT = 0;
+let firsttime= true;
 
 // Fix typos/undefined globals referenced later
 let corruptedP = 0; // speed modifier
@@ -89,6 +90,7 @@ const newLoader = new THREE.GLTFLoader();
    addCollider(1,18.5,15,3);
    addCollider(2,3,8,18);
    addCollider(1,18,22.5,8.2);
+   addCollider(2,7.4,3,22.2);
    addCollider(1,22.2,33.3,3);
    addCollider(2,3,-7,33.3);
    addCollider(1,33.3,15,-6.5);
@@ -112,7 +114,6 @@ function addCollider(xorz, start, end, other, y=1000) {
         );
         colliderx.position.set(midpoint, 0, other);
         colliders.push(colliderx);
-        colliderx.visible = false;
         scene.add(colliderx); // Crucial step
     } else if (xorz === 2) {
         const colliderz = new THREE.Mesh(
@@ -121,7 +122,6 @@ function addCollider(xorz, start, end, other, y=1000) {
         );
         colliderz.position.set(other, 0, midpoint);
         colliders.push(colliderz);
-        colliderz.visible = false;
         scene.add(colliderz);
     }
 }
@@ -156,7 +156,7 @@ function flashObserver() {
 
 function startGame() {
 
-  window.alert("version 0.1419");
+  window.alert("version 0.1420");
 
  resetGameState();
  LVL1();
@@ -557,6 +557,11 @@ if (intersects.length > 0) {
      FLASHLIGHTON = true;
    }
  }
+
+if (camera.position.x >= 15 && firsttime) {
+  window.alert("inside of house");
+  firsttime = false;
+}
 
  renderer.render(scene, camera);
 }
