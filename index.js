0dcc7d5802bbc42e790d36f923bdc50176de541b
darkstalkerhansen-jpg <darkstalkerhansen@gmail.com>
2026-05-17 20:22:20 -0600

    test 6.8

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function flashObserver() {
 
 function startGame() {
 
-  window.alert("version 0.1406.6");
+  window.alert("version 0.1406.8");
 
  resetGameState();
  LVL1();
@@ -201,83 +201,7 @@ function loadWalls(iteration=1) {
 
 
   const loader = new THREE.GLTFLoader();
- loader.load('./tree.glb', (gltf) => {
-   const treea = gltf.scene;
-   treea.scale.setScalar(0.1); // Reduces size by a factor of 10
-
-    // Adjusted smaller size
-   treea.position.set(5,0,-5);
-  
-   // Add emissive red glow effect
-  
-   trunk = new THREE.Mesh(
-  new THREE.BoxGeometry(1,10,1)
-   );
-
- trunk.position.set(5,0,-5);
- trunk.visible = true;
- colliders.push(trunk);
- scene.add(trunk);
-
- const ambientLight = new THREE.AmbientLight(0xffffff, 0.7); // Soft white light
-scene.add(ambientLight);
-
- const tree = new THREE.Group();
- tree.add(treea);
- tree.add(ambientLight);
- colliders.pop(tree);
- const tree1 = tree.clone();
- tree1.position.set(-5,0,-5);
-  
- scene.add(tree1);
- const tree2 = tree.clone();
- tree2.position.set(5,0,-5);
-  
- scene.add(tree2);
- const tree3 = tree.clone();
- tree3.position.set(5,0,8);
-  
- scene.add(tree3);
-
-
- const tee = new THREE.Group();
- tee.add(trunk);
- colliders.push(tee);
- const tee1 = tee.clone();
- tee1.position.set(-5,0,-5);
- colliders.push(tee1); 
- scene.add(tee1);
- const tee2 = tree.clone();
- tee2.position.set(5,0,-5);
-  colliders.push(tee2) 
  
- scene.add(tee2);
- const tee3 = tee.clone();
- tee3.position.set(5,0,8);
-  colliders.push(tee3); 
- 
- scene.add(tee3);
-
- 
- 
- const treecluster = new THREE.Group();
- treecluster.add(tree1);
- treecluster.add(tree2);
- treecluster.add(tree3);
- treecluster.position.set(-10,0,5);
- scene.add(treecluster);
-
- const teecluster = new THREE.Group();
- teecluster.add(tee);
- teecluster.add(tee1);
- teecluster.add(tee2);
- teecluster.add(tee3);
- teecluster.position.set(-10,0,5);
- scene.add(teecluster);
- 
- 
- });
-
  loader.load('./tree2.glb', (gltf) => {
    const zreea = gltf.scene;
    zreea.scale.setScalar(0.1); // Reduces size by a factor of 10
